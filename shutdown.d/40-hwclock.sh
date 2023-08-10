512ff7ed753c94daede4418183620a6808b2fdb7
Fabio Santos <fabioesantos.bio@gmail.com>
2023-08-10 13:24:08 -0300

    better hwclock shotdown script

--- shutdown.d/40-hwclock.sh
+++ shutdown.d/40-hwclock.sh
@@ -1,2 +1,4 @@
-msg "Saving hardware clock..."
-hwclock --systohc --utc
+if [ -n "$HARDWARECLOCK" ]; then
+    msg "Saving hardware clock..."
+    hwclock --systohc ${HARDWARECLOCK:+--$(echo $HARDWARECLOCK |tr A-Z a-z)}
+fi
